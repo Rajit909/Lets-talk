70cd6f2106a1bb887921ce573b8bbaf16e486ea5
rajit909 <krajit909@gmail.com>
2025-09-07 15:33:07 +0530

    bug fix

--- server/user/src/lib/config/genrateToken.ts
+++ server/user/src/lib/config/genrateToken.ts
@@ -1,8 +1,10 @@
- import dotenv from "dotenv"
+//  import dotenv from "dotenv"
 
- dotenv.config();
+//  dotenv.config();
 
- const JWT_SECRET = process.env.JWT_SECRET;
+//  const JWT_SECRET = process.env.JWT_SECRET;
 
 
- export const genrateToken = 
+//  export const genrateToken = ()=>{
+
+//  }
